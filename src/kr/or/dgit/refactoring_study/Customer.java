9c4c06cc9c0c5b5445378fafcbca6e259cbe5ced
KangHyun Lee <rkd7327@naver.com>
2017-12-05 16:12:25 +0900

    메서드 추출

--- src/kr/or/dgit/refactoring_study/Customer.java
+++ src/kr/or/dgit/refactoring_study/Customer.java
@@ -26,40 +26,17 @@ public class Customer {
 		StringBuilder result = new StringBuilder(getName() + " 고객님의 대여 기록\n");
 
 		for (Rental each : rentals) {
-			double thisAmout = 0;
-
-			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmout += 2;
-				if (each.getDaysRented() > 2) {
-					thisAmout += (each.getDaysRented() - 2) * 1.5;
-				}
-				break;
-			case Movie.NEW_RELEASE:
-
-				thisAmout += each.getDaysRented() * 3;
-
-				break;
-			case Movie.CHILDRENS:
-				thisAmout += 1.5;
-				if (each.getDaysRented() > 3) {
-					thisAmout += (each.getDaysRented() - 3) * 1.5;
-				}
-				break;
-			}
-			frequentRenterPoints++;
-
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-				frequentRenterPoints++;
-
+			frequentRenterPoints += each.getFrequenRenterPoints();
+			
 			result.append("\t" + each.getMovie().getTitle());
-			result.append("\t" + String.valueOf(thisAmout) + "\n");
+			result.append("\t" + String.valueOf(each.getCharge()) + "\n");
 
-			totalAmount += thisAmout;
-
+			totalAmount += each.getCharge();
 		}
 		result.append("누적 대여료 : " + String.valueOf(totalAmount) + "\n");
 		result.append("적립 포인트 : " + String.valueOf(frequentRenterPoints));
+		
 		return result.toString();
 	}
+
 }
--- src/kr/or/dgit/refactoring_study/Rental.java
+++ src/kr/or/dgit/refactoring_study/Rental.java
@@ -17,5 +17,36 @@ public class Rental {
 	public int getDaysRented() {
 		return daysRented;
 	}
+	
+	public double getCharge() {
+		double result = 0;
+		switch (getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			result += 2;
+			if (getDaysRented() > 2) {
+				result += (getDaysRented() - 2) * 1.5;
+			}
+			break;
+		case Movie.NEW_RELEASE:
+
+			result += getDaysRented() * 3;
+
+			break;
+		case Movie.CHILDRENS:
+			result += 1.5;
+			if (getDaysRented() > 3) {
+				result += (getDaysRented() - 3) * 1.5;
+			}
+			break;
+		}
+		return result;
+	}
+	
+	public int getFrequenRenterPoints() {
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
+			return 2;
+		else
+			return 1;
+	}
 
 }
